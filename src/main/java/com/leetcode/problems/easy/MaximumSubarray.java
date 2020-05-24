caf1d2e7614b37659fb8d567e235cbb1ae8d0d5f
liangwei <372856634@qq.com>
2020-05-24 15:22:59 +0800

    增加注释

--- src/main/java/com/leetcode/problems/easy/MaximumSubarray.java
+++ src/main/java/com/leetcode/problems/easy/MaximumSubarray.java
@@ -22,6 +22,7 @@ public class MaximumSubarray {
         int maxSum = nums[0];
         int currentSum = nums[0];
         for(int i =1;i<nums.length;i++){
+            // f(n) = max(f(n-1)+nums[n],nums[n)
             currentSum = Math.max(currentSum+nums[i],nums[i]);
             maxSum = Math.max(maxSum,currentSum);
         }
