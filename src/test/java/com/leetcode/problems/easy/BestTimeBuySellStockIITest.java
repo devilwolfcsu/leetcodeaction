788e3af6cce0bbf447bbd2250a19852202587362
liangwei <372856634@qq.com>
2020-05-24 15:00:54 +0800

    122. 买卖股票的最佳时机 解答与测试

---
+++ src/test/java/com/leetcode/problems/easy/BestTimeBuySellStockIITest.java
@@ -0,0 +1,25 @@
+package com.leetcode.problems.easy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**BestTimeBuySellStockII 测试
+ * Created by zeyuan on 2020/5/24.
+ */
+public class BestTimeBuySellStockIITest {
+
+    @Test
+    public void maxProfit(){
+
+        BestTimeBuySellStockII bestTimeBuySellStockII = new BestTimeBuySellStockII();
+        int[] stocks = {7,1,5,3,6,4};
+        Assert.assertEquals(bestTimeBuySellStockII.maxProfit(stocks),7);
+
+        int[] newStocks ={1,2,3,4,5};
+        Assert.assertEquals(bestTimeBuySellStockII.maxProfit(newStocks),4);
+
+        int[] otherStocks ={7,6,4,3,1};
+        Assert.assertEquals(bestTimeBuySellStockII.maxProfit(otherStocks),0);
+
+    }
+}
