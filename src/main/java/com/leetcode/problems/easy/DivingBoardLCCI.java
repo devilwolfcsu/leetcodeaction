e3349baea3f9be01ad70b819fc15f2045f5f4515
liangwei <372856634@qq.com>
2020-07-23 22:46:54 +0800

    跳水板   题解& 测试

---
+++ src/main/java/com/leetcode/problems/easy/DivingBoardLCCI.java
@@ -0,0 +1,40 @@
+package com.leetcode.problems.easy;
+
+import java.util.Set;
+
+/**
+ * 面试题 16.11. 跳水板 Diving Board LCCI
+ *
+ * 你正在使用一堆木板建造跳水板。有两种类型的木板，其中长度较短的木板长度为shorter，长度较长的木板长度为longer。
+ * 你必须正好使用k块木板。编写一个方法，生成跳水板所有可能的长度。返回的长度需要从小到大排列。
+ *
+ * 示例1
+ *
+ * 输入：
+ * shorter = 1
+ * longer = 2
+ * k = 3
+ * 输出： [3,4,5,6]
+ * 解释：
+ *  可以使用 3 次 shorter，得到结果 3；使用 2 次 shorter 和 1 次 longer，得到结果 4 。以此类推，得到最终结果。
+ * Created by zeyuan on 2020/7/23.
+ */
+public class DivingBoardLCCI {
+
+    private Set<Integer> result;
+
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        if (shorter == longer) {
+            return new int[]{shorter * k};
+        }
+        int[] lengths = new int[k + 1];
+        for (int i = 0; i <= k; i++) {
+            lengths[i] = shorter * (k - i) + longer * i;
+        }
+        return lengths;
+    }
+
+}
